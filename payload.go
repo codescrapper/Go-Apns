863dbb86b6f150448750f6dc55ca4d69c7d38f22
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Pro.local>
2015-12-20 16:44:11 +0530

    urlargs for safari

--- payload.go
+++ payload.go
@@ -19,6 +19,7 @@ type Aps struct {
 	Badge int    `json:"badge,omitempty"`
 	Sound string `json:"sound,omitempty"`
 	Category string `json:"category,omitempty"`
+	Urlargs []string `json:"url-args,omitempty"`
 }
 
 type Payload struct {
